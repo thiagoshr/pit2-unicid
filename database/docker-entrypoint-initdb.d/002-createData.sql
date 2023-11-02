22dabad8aba56b22e952b5f8b431dc96779420e0
Thiago Herrera <thiagoh@tutanota.com>
2023-11-02 17:31:48 -0300

    mock users for every use case

---
+++ database/docker-entrypoint-initdb.d/002-createData.sql
@@ -0,0 +1,26 @@
+INSERT INTO usuario (nome, username, senha, tipo) VALUES 
+	(
+		'Barista',
+		'barista',
+		'$2b$16$x0R.8/Is/kR7FKmT849J1ejg7I01On/oxlfzCgvPRN5Vet3zL1aa6',
+		1
+	),
+	(
+		'Gerente',
+		'gerente',
+		'$2b$16$vfNdOtXZN5/7Ez.eGrnSRuPJXO.4t6V9GC3mkzqtNCk6L3v3hTp1y',
+		2
+	),
+	(
+		'Operador de Caixa',
+		'caixa',
+		'$2b$16$1o.4bENYctEf2KGmh9BivO6T3CTP06bn6h9iGhi9TER8i8Md3Haiu',
+		3
+	),
+	(
+		'Cliente',
+		'cliente',
+		'$2b$16$KuHDCZ1t0m992tND6zkO8um86OCTT7t9LEIpGJesXm.i8jIx8lGJe',
+		4
+	)
+;
