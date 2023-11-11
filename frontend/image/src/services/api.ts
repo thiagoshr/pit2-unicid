ee7b058b04ca31a3a649a1c61dc402f73523f71e
Thiago Herrera <thiagoh@tutanota.com>
2023-11-11 13:19:56 -0300

    removing unused types

--- frontend/image/src/services/api.ts
+++ frontend/image/src/services/api.ts
@@ -1,26 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export interface QueryParams  {
-	url : string,
-	params : any
-};
-
-export interface CafeteriaApiBasics {
-	success : boolean
-};
-
-export interface SuccessApiResponse<T> extends CafeteriaApiBasics {
-	data : Array<T>,
-	error? : never
-};
-
-export interface FailApiResponse extends CafeteriaApiBasics {
-	data? : never,
-	error: string
-}
-
-export type CafeteriaApiResponse<T> = SuccessApiResponse<T> | FailApiResponse;
-
 export const API_TAG_LOGIN = 'login-data';
 
 export const cafeteriaApi = createApi({
