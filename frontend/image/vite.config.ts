d0f3fa37a3e4da7720ba734587ac4ade298e4299
Thiago Herrera <thiagoh@tutanota.com>
2023-11-11 15:03:00 -0300

    working frontend container

--- frontend/image/vite.config.ts
+++ frontend/image/vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: false,
+    host: true
   },
   build: {
     outDir: "build",
