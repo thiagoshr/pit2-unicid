4329100e5b78386bbbeff9095907cea36997f93c
Thiago Herrera <thiagoh@tutanota.com>
2023-11-15 15:38:21 -0300

    DB simplification

--- database/docker-entrypoint-initdb.d/001-createSchema.sql
+++ database/docker-entrypoint-initdb.d/001-createSchema.sql
@@ -10,13 +10,6 @@ CREATE TABLE mesa (
 	capacidade_pessoas INT NOT NULL DEFAULT 1
 );
 
-CREATE TABLE ingrediente (
-	id BIGSERIAL PRIMARY KEY,
-	nome TEXT NOT NULL,
-	qtd_minima_estoque BIGINT NOT NULL DEFAULT 0,
-	unidade TEXT NOT NULL
-);
-
 CREATE TABLE usuario (
 	id BIGSERIAL PRIMARY KEY,
 	nome TEXT NOT NULL,
@@ -69,23 +62,6 @@ CREATE TABLE receita (
 	id_produto BIGINT NOT NULL,
 	modo_preparo TEXT NOT NULL,
 	FOREIGN KEY (id_produto) REFERENCES produto(id)
-);
-
-CREATE TABLE receita_ingredientes (
-	id_receita BIGINT,
-	id_ingrediente BIGINT,
-	quantidade BIGINT NOT NULL,
-	unidade TEXT NOT NULL,
-	PRIMARY KEY (id_receita, id_ingrediente),
-	FOREIGN KEY (id_receita) REFERENCES receita(id),
-	FOREIGN KEY (id_ingrediente) REFERENCES ingrediente(id)
-);
-
-CREATE TABLE estoque (
-	id_ingrediente BIGINT PRIMARY KEY,
-	unidade TEXT NOT NULL,
-	quantidade BIGINT NOT NULL DEFAULT 0,
-	FOREIGN KEY (id_ingrediente) REFERENCES ingrediente(id)
 );
 
 CREATE TABLE turno (
