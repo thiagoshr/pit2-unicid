e412fe12fdbe83f42decdcbb5ecacd8fb781a730
Thiago Herrera <thiagoh@tutanota.com>
2023-11-19 16:56:14 -0300

    inicial product config and file upload backend infrastructure

--- backend/src/run/wsgi.py
+++ backend/src/run/wsgi.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
+from werkzeug.utils import secure_filename
 
 from auth.authEndpoints import authEndpoints
 
 def create_app():
+	UPLOAD_FOLDER = '/opt/cafeteria/images'
+	ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'gif', 'png'}
+
 	app = Flask(__name__)
+	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+	app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 # 2 MiB file size limit
 
 	app.url_map.strict_slashes = False
 
